a63916760454abe39b16a50b29bdafa9e2d97f4e
Azfar Nayyan <azfarnayyan@192.168.1.10>
2026-08-23 16:07:17 +0500

    deployment/files-for-deployment

--- my-toblerone/config/cors.php
+++
@@ -1,7 +0,0 @@
-'paths' => ['api/*'],
-'allowed_methods' => ['*'],
-'allowed_origins' => [
-    'https://your-frontend.vercel.app', // add your real Vercel URL once you have it
-],
-'allowed_headers' => ['*'],
-'supports_credentials' => false,
